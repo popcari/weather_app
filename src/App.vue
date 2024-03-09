84e9c8f26aa76fff6e2d4a6b8602782150d8b782
popcari <duy.nt1009@gmail.com>
2024-03-09 11:03:21 +0700

    layout/navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen text-white bg-teal-300">
+    <SiteNavivation></SiteNavivation>
     <RouterView></RouterView>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import SiteNavivation from './components/SiteNavivation.vue'
+</script>
 
 <style scoped></style>
---
+++ src/components/SiteNavivation.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="sticky top-0 bg-teal-400 shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 py-4 text-white">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-2 flex-1">
+          <i class="fa-solid fa-snowflake text-xl"></i>
+          <p class="text-xl">The local weather</p>
+        </div>
+      </RouterLink>
+      <div class="flex gap-2 flex-1 justify-end">
+        <i class="fa-solid fa-circle-info hover:text-teal-500 duration-300 cursor-pointer"></i>
+        <i class="fa-solid fa-plus hover:text-teal-500 duration-300 cursor-pointer"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
